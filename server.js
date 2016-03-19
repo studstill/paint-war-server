cc581ffc7e5e9b1a6fdc6be538642d6458ecdd08
muldrewd <muldrewd@yahoo.com>
2016-03-19 12:41:51 -0700

    added equirectangular distance function

--- server.js
+++ server.js
@@ -6,6 +6,23 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var PORT = process.env.PORT || 3000;
 
+// convert numeric degress to radians
+toRad = function (value) {
+    return value * Math.PI / 180;
+}
+
+// simple distance calculation
+// works fine for small distances
+// http://en.wikipedia.org/wiki/Equirectangular_projection
+Equirectangular = function(point1, point2) {
+    var R = 6371; // earth radius in km
+    var x = (toRad(point2.lon)-toRad(point1.lon)) *
+            Math.cos((toRad(point1.lat)+toRad(point2.lat))/2);
+    var y = (toRad(point2.lat)-toRad(point1.lat));
+    return Math.sqrt(x*x + y*y) * R;
+};
+
+
 server.listen(PORT, function() {
   console.log('Server listening at PORT ' + PORT);
 });
