900541db5177c89a6c39670442f92c7d29485983
Jason Studstill <jason@jasonstudstill.com>
2016-03-19 17:19:34 -0700

    Update index.html

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.get('/randomPark', function(req, res) {
   // res.json(trailsData);
 
 app.post('/location', function(req, res) {
-  console.log(res.body);
+  console.log(req.body);
   res.json(res.body);
   res.end();
 
